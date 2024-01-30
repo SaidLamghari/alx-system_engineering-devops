30be547223ce7d255bba3392996e7a31aa98e0d7
SaidLamghari <said.lmg@gmail.com>
2024-01-30 09:27:27 -0800

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,14 +1,4 @@
 #!/usr/bin/env ruby
 # Task 0: Simply matching School
 
-in_put = ARGV[0]
-
-rgx = /School/
-
-match = in_put.match(rgx)
-
-if match
-  puts "School$"
-else
-  puts ""
-end
+puts ARGV[0].scan(/School/).join
