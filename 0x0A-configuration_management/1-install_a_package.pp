d38bd91948fa8e1d26f3cbba53a774ebbc49f0af
SaidLamghari <said.lmg@gmail.com>
2024-03-22 18:17:29 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,5 @@
-# Puppet Manifest: 1-install_a_package.pp
-#
-# This installs Flask using pip3 with the specified version.
-#
-# Requirements:
-#   - Package: Flask
-#   - Version: 2.1.0
-#
-# Author: Said Lamghari
-package { 'Flask':
+# Flask version 2.1.0 use pip
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
 }
-
