5862ffc885646f60bf50acca19e530c1e5168b2f
SaidLamghari <said.lmg@gmail.com>
2024-04-01 23:16:19 +0000

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -18,32 +18,6 @@ file { '/etc/nginx/sites-available/default':
   source => 'puppet:///modules/nginx/default',
 }
 
-# Create HTML file
-file { '/var/www/html/index.html':
-  ensure => present,
-  content => 'Hello World!',
-}
-
-# Configure Redir.
-file { '/etc/nginx/sites-available/default':
-  ensure => present,
-  mode => '0644',
-  content => template('nginx/custom_redirect.erb'),
-}
-
-# Create 404 HTML file
-file { '/var/www/html/404.html':
-  ensure => present,
-  content => "Ceci n'est pas une page",
-}
-
-# Configure Nginx to use 404 page
-file { '/etc/nginx/sites-available/default':
-  ensure => present,
-  mode => '0644',
-  content => template('nginx/custom_404.erb'),
-}
-
 # Define header
 $hostname = $::fqdn
 
@@ -66,3 +40,4 @@ service { 'nginx':
   enable => true,
   require => [File['/etc/nginx/sites-available/default'], Exec['nginx_reload']],
 }
+
