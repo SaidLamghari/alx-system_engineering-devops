4117fab1596f33f48b96d6595e24db719ef31690
SaidLamghari <said.lmg@gmail.com>
2024-04-22 15:12:06 -0700

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python3
-"""
-Un script Python qui, en utilisant une API REST donnée,
-récupère des informations sur
-l'avancement de la liste de tâches d'un employé
-les exporte au format CSV.
-Autor: Said LAMGHARI
-"""
+"""Exports to-do list information for a given employee ID to CSV format."""
 
 import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-    
-    # URL de base pour l'API JSONPlaceholder
-    base_url = "https://jsonplaceholder.typicode.com/"
 
-    # Obtenir les informations sur
-    # l'employé en utilisant l'ID d'employé fourni
-    user_id = sys.argv[1]
-    user_info = requests.get(base_url + "users/{}".format(user_id)).json()
-    username = user_info.get("username")
-
-    # Obtenir la liste de tâches à faire
-    # pour l'employé en utilisant l'ID d'employé fourni
-    todos = requests.get(base_url + "todos", params={"userId": user_id}).json()
-
-    # Écrire les données dans un fichier CSV
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+def export_to_csv(user_id, username, tasks):
+    filename = f"{user_id}.csv"
+    with open(filename, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for todo in todos:
-            writer.writerow([user_id, username, todo.get("completed"), todo.get("title")])
+        for task in tasks:
+            writer.writerow([user_id, username, task["completed"], task["title"]])
 
+
+if __name__ == "__main__":
+    # Get the user ID from the command-line arguments provided to the script
+    user_id = sys.argv[1]
+
+    # Define the base URL for the JSON API
+    url = "https://jsonplaceholder.typicode.com/"
+
+    # Fetch user information from the API and
+    #   convert the response to a JSON object
+    user = requests.get(url + "users/{}".format(user_id)).json()
+
+    # Extract the username from the user data
+    username = user.get("username")
+
+    # Fetch the to-do list items associated with the
+    #   given user ID and convert the response to a JSON object
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    # Export the to-do list items to a CSV file
+    export_to_csv(user_id, username, todos)
