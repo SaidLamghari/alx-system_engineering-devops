0e3f54d012753b3d66957ef32dee2184fa6d68d7
SaidLamghari <said.lmg@gmail.com>
2024-04-22 14:37:31 -0700

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,12 +11,12 @@ import csv
 import requests
 import sys
 
-
 if __name__ == "__main__":
+    
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>")
         sys.exit(1)
-
+    
     # URL de base pour l'API JSONPlaceholder
     base_url = "https://jsonplaceholder.typicode.com/"
 
@@ -45,3 +45,28 @@ if __name__ == "__main__":
                 str(tsk.get("completed")),
                 tsk.get("title")
             ])
+    
+    # Vérifier l'ID utilisateur et le nom d'utilisateur dans le fichier CSV
+    user_ids_from_csv = set()
+    usernames_from_csv = set()
+
+    with open("{}.csv".format(id_employe), "r", newline="") as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # Skip header row
+        for row in reader:
+            user_ids_from_csv.add(row[0])
+            usernames_from_csv.add(row[1])
+
+    if id_employe in user_ids_from_csv and infs_emply.get("username") in usernames_from_csv:
+        print("User ID and Username: OK")
+    else:
+        print("User ID: Incorrect / Username: Incorrect")
+    
+    # Vérifier le nombre de tâches dans le fichier CSV
+    with open("{}.csv".format(id_employe), "r", newline="") as csvfile:
+        task_count = sum(1 for row in csvfile) - 1  # Subtract header row
+        if task_count == len(todo_lst):
+            print("Number of tasks in CSV: OK")
+        else:
+            print("Number of tasks in CSV: Incorrect")
+
