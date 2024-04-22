799373fee9c2f2f0a8c79d67a326fbcd7d348525
SaidLamghari <said.lmg@gmail.com>
2024-04-22 12:03:14 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,55 +10,31 @@ import requests
 import sys
 
 
-def gt_todo_prgrss(employee_id):
-    """
-    Récupère et affiche les informations
-    sur l'avancement de la liste
-    de tâches d'un employé.
-
-    Args: employee_id (int): L'ID de l'employé.
-
-    Returns: None
-    """
-    # URL de base pour l'API JSONPlaceholder
-    base_lnk = "https://jsonplaceholder.typicode.com/"
-
-    try:
-        # Obtenir les informations sur l'employé
-        # en utilisant l'ID d'employé fourni
-        guser = requests.get(base_lnk + "users/{}".format(employee_id)).json()
-
-        # Obtenir la liste de tâches de
-        # l'employé en utilisant l'ID d'employé fourni
-        params = {"userId": employee_id}
-        todos = requests.get(base_lnk + "todos", params=params).json()
-
-        # Filtrer les tâches terminées et les compter
-        completed = [t.get("title") for t in todos if t.get("completed")]
-
-        # Afficher le nom de l'employé et le nombre de tâches terminées
-        print("Employee {} is done with tasks({}/{}):".format(
-            guser.get("name"), len(completed), len(todos)))
-
-        # Afficher les tâches terminées une par une avec indentation
-        # Afficher les tâches terminées une par une avec indentation
-        for i, complete in enumerate(completed, start=1):
-            print("Task {} Formatting: OK".format(i))
-
-    except requests.exceptions.RequestException as e:
-        # Gérer les erreurs de requête
-        print(f"Erreur : {e}")
-        sys.exit(1)
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Utilisation : python3 nom_du_script.py <id_employé>")
-        sys.exit(1)
+    # URL de base pour l'API JSONPlaceholder
+    base_url = "https://jsonplaceholder.typicode.com/"
 
-    employee_id = sys.argv[1]
-    if not employee_id.isdigit():
-        print("Erreur : L'ID de l'employé doit être un entier.")
-        sys.exit(1)
+    # Obtenir les informations sur
+    # l'employé en utilisant l'ID d'employé fourni
+    id_employe = sys.argv[1]
+    infs_emply = requests.get(base_url + "users/{}".format(id_employe)).json()
 
-    gt_todo_prgrss(int(employee_id))
+    # Obtenir la liste de tâches à faire
+    # pour l'employé en utilisant l'ID d'employé fourni
+    todo_prms = {"userId": id_employe}
+    todo_list = requests.get(base_url + "todos", todo_prms).json()
+
+    # Filtrer les tâches terminées et les compter
+    taches_terminees = []
+    for tache in todo_list:
+        if tache.get("completed"):
+            taches_terminees.append(tache.get("title"))
+
+    # Afficher le nom de l'employé
+    # le nombre de tâches terminées
+    print("Employee {} is done with tasks({}/{}):".format(
+        infs_emply.get("name"), len(taches_terminees), len(todo_list)))
+
+    # Afficher les tâches terminées une par une avec indentation
+    for task_title in taches_terminees:
+        print("\t", task_title)
