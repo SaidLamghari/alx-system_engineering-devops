b5246e5d6c0d05373bda18764f53b30c456b6a8f
SaidLamghari <said.lmg@gmail.com>
2024-05-16 12:30:44 -0700

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,20 @@
 # Puppet manifeste pour corriger la configuration de Nginx afin d'avoir zéro requête échouée
 # Auteur : SAID LAMGHARI
 
-exec { 'nginx_and_fix_limit_setting':
-  # Commande pour modifier la configuration de Nginx et redémarrer le service
-  command     => "sed -i 's/-n 15/-n 4096/' /etc/default/nginx && service nginx restart",
-  # Vérifie si le fichier /etc/default/nginx existe et si la configuration par défaut contient "-n 15"
-  onlyif      => 'test -e /etc/default/nginx && grep "-n 15" /etc/default/nginx',
-  # Chemin d'accès aux exécutables requis par la commande
-  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
-  # Ne s'exécute que lorsqu'il est notifié par une autre ressource
-  refreshonly => true,
+# Puppet manifest pour modifier le paramètre de limite maximale de fichiers ouverts
+exec {'modify_max_open_files_limit':
+  # Modifie la limite maximale de fichiers ouverts
+  command => 'sed -i "s/15/10000/" /etc/default/nginx',
+  # Commande pour modifier le fichier de configuration de Nginx
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+  # Chemin d'accès aux exécutables requis
 }
+
+# Redémarre le service Nginx
+exec {'restart_nginx_service':
+  # Commande pour redémarrer le service Nginx
+  command => 'sudo service nginx restart',
+  # Chemin d'accès aux exécutables requis
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
+
