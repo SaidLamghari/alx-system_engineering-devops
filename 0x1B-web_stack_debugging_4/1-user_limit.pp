0a752fbf68a909a026240136722549bd17a2cc9d
SaidLamghari <said.lmg@gmail.com>
2024-05-16 13:13:45 -0700

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,11 +3,28 @@
 # de fichiers ouverts pour l'utilisateur holberton
 # Autor SAID LAMGHARI
 
-exec { 'change_user_limit':
-  # Modifie la limite du nombre maximal
-  # de fichiers ouverts pour l'utilisateur holberton
-  command => 'ulimit -n 4096',
-  # Commande à exécuter pour modifier la limite
-  user    => 'holberton',
-  # Utilisateur auquel appliquer la modification
+# Ouvrir un fichier sans message d'erreur.
+
+file { '/etc/security/limits.conf':
+  # Chemin du fichier de configuration à modifier
+  ensure  => present,
+  # Assure que le fichier existe
+
+  content => template('your_module/limits.conf.erb'),
+  # Contenu du fichier basé sur un modèle,
+  # où 'your_module/limits.conf.erb' est le chemin vers le fichier modèle
+
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  # Propriétaire, groupe et permissions du fichier
 }
+
+exec { 'reload_limits_conf':
+  # Exécuter la commande pour recharger la configuration des limites
+  command     => 'sysctl -p',
+  # La commande pour recharger la configuration
+  refreshonly => true,
+  # Ne s'exécute que si une notification est reçue
+}
+
